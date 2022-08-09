19e1e1a5c17ee080cf6a094af825f2d3dd2797da
Vanessa <vandlopes98@gmail.com>
2022-08-09 19:25:33 -0300

    Created components module

---
+++ solutions/devsprint-bruno-ramos-6/Modules/Package.swift
@@ -0,0 +1,22 @@
+// swift-tools-version:5.5
+
+import PackageDescription
+
+let package = Package(
+    name: "Modules",
+    platforms: [.iOS(.v14)],
+    products: [
+        .library(
+            name: "Modules",
+            targets: ["Components"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "Components",
+            dependencies: []),
+        .testTarget(
+            name: "ComponentsTests",
+            dependencies: ["Components"]),
+    ]
+)
---
+++ solutions/devsprint-bruno-ramos-6/Modules/Sources/Components/Modules.swift
@@ -0,0 +1,5 @@
+public struct Modules {
+    public private(set) var text = "Hello, World!"
+
+    public init() { }
+}
