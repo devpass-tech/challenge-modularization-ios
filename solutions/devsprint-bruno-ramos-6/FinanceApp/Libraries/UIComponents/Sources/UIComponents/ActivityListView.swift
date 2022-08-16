56ea48b4a1bcb02a5e164db6d93a08523a87e195
Pedro Francisco Tres <52545592+pedrotres@users.noreply.github.com>
2022-08-15 21:18:56 -0300

    fix home module

--- solutions/devsprint-bruno-ramos-6/FinanceApp/Libraries/UIComponents/Sources/UIComponents/ActivityListView.swift
+++ solutions/devsprint-bruno-ramos-6/FinanceApp/Libraries/UIComponents/Sources/UIComponents/ActivityListView.swift
@@ -7,20 +7,20 @@
 
 import UIKit
 
-protocol ActivityListViewDelegate: AnyObject {
+public protocol ActivityListViewDelegate: AnyObject {
 
     func didSelectedActivity()
 }
 
-class ActivityListView: UIView {
+public class ActivityListView: UIView {
 
-    weak var delegate: ActivityListViewDelegate?
+    public weak var delegate: ActivityListViewDelegate?
 
-    static let cellSize = CGFloat(82)
+    public static let cellSize = CGFloat(82)
 
     private let cellIdentifier = "ActivityCellIdentifier"
 
-    lazy var tableView: UITableView = {
+    public lazy var tableView: UITableView = {
 
         let tableView = UITableView(frame: .zero)
         tableView.translatesAutoresizingMaskIntoConstraints = false
@@ -30,7 +30,7 @@ class ActivityListView: UIView {
         return tableView
     }()
 
-    init() {
+    public init() {
         super.init(frame: .zero)
 
         backgroundColor = .white
@@ -81,7 +81,7 @@ extension ActivityListView: UITableViewDataSource {
 
 extension ActivityListView: UITableViewDelegate {
 
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    public func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return ActivityListView.cellSize
     }
 
