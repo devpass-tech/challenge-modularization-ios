16a2d3896c3b9c52b66cd7d9d93bbaacf3573710
Gustavo Fernandes <gustavofernandba@gmail.com>
2022-11-08 15:28:43 -0300

    Criando componente de Transfers

---
+++ solutions/devsprint-julio-fernandes-7/libraries/Transfers/TransfersTests/TransfersInitializerTests.swift
@@ -0,0 +1,15 @@
+//
+//  TransfersInitializerTests.swift
+//  TransfersTests
+//
+
+import XCTest
+@testable import Transfers
+
+final class TransfersInitializerTests: XCTestCase {
+    
+    func testHellorWorld() {
+        XCTAssertEqual(TransfersInitializer.helloWorld(), "Hello World from SDK!!")
+    }
+    
+}
