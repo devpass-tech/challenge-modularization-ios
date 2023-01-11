b54c0844e4fac7d633b7ea5aa41bbf9ce248a4ae
Julio Fernandes <julio.fernandes7@gmail.com>
2023-01-11 18:27:28 -0300

    criando ContactList SDK e ajustando dependências

--- solutions/devsprint-julio-fernandes-8/FinanceApp/AppDelegate/SceneDelegate.swift
+++ solutions/devsprint-julio-fernandes-8/FinanceApp/AppDelegate/SceneDelegate.swift
@@ -1,11 +1,11 @@
 import UIKit
 import Components
 import Home
+import Transfers
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
 
