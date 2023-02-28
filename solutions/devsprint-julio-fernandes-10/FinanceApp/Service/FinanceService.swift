a84a3f8795509038e7062bb3f0d4a8bd362b63d0
Thonatas Reis Borges <premiersoft-thonatas.borges@ailos.coop.br>
2023-02-27 21:45:29 -0400

    Add Activity Details endpoint

--- solutions/devsprint-julio-fernandes-10/FinanceApp/Service/FinanceService.swift
+++ solutions/devsprint-julio-fernandes-10/FinanceApp/Service/FinanceService.swift
@@ -9,6 +9,7 @@ import Foundation
 
 enum FinanceEndpoint: String {
     case home = "home_endpoint"
+    case activityDetails = "activity_details_endpoint"
     case contactList = "contact_list_endpoint"
     case userProfile = "user_profile_endpoint"
     case transfer = "transfer_successful_endpoint"
