6062d1fc52b70f5318eb1b20feed46e977a353eb
Thonatas Reis Borges <premiersoft-thonatas.borges@ailos.coop.br>
2023-02-28 09:14:44 -0400

    Add Service Error Enum

--- solutions/devsprint-julio-fernandes-10/FinanceApp/Service/FinanceService.swift
+++ solutions/devsprint-julio-fernandes-10/FinanceApp/Service/FinanceService.swift
@@ -7,6 +7,12 @@
 
 import Foundation
 
+enum FinanceServiceError: Error {
+    case invalidURL
+    case invalidData
+    case parse
+}
+
 enum FinanceEndpoint: String {
     case home = "home_endpoint"
     case activityDetails = "activity_details_endpoint"
@@ -18,35 +24,23 @@ enum FinanceEndpoint: String {
 final class FinanceService {
     private let baseURL = "https://raw.githubusercontent.com/devpass-tech/challenge-finance-app/main/api/"
     
-    func fetchData<T: Decodable>(_: T.Type, endpoint: FinanceEndpoint, _ completion: @escaping (T?) -> Void) {
+    func fetchData<T: Decodable>(_: T.Type, endpoint: FinanceEndpoint, _ completion: @escaping (Result<T, Error>) -> Void) {
         guard let url = URL(string: baseURL + endpoint.rawValue + ".json") else {
-            print("Invalid URL")
-            completion(nil)
+            completion(.failure(FinanceServiceError.invalidURL))
             return
         }
 
         let dataTask = URLSession.shared.dataTask(with: url) { data, response, error in
-            guard error == nil else {
-                completion(nil)
-                return
-            }
-
             guard let data = data else {
-                print("Invalid Data")
-                completion(nil)
+                completion(.failure(FinanceServiceError.invalidData))
                 return
             }
-
-            do {
-                let decoder = JSONDecoder()
-                decoder.keyDecodingStrategy = .convertFromSnakeCase
-                
-                let result: T = try decoder.decode(T.self, from: data)
-                completion(result)
-            } catch {
-                print(error)
-                completion(nil)
+            guard let decodedData = try? JSONDecoder().decode(T.self, from: data) else {
+                completion(.failure(FinanceServiceError.parse))
+                return
             }
+            
+            completion(.success(decodedData))
         }
 
         dataTask.resume()
