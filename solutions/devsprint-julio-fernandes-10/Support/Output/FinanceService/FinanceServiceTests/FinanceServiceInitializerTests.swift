eb6e0be41f8a8f53bef16e401f618a32294962f2
Thonatas Reis Borges <premiersoft-thonatas.borges@ailos.coop.br>
2023-03-02 11:32:57 -0400

    Create Files with Rake Script

---
+++ solutions/devsprint-julio-fernandes-10/Support/Output/FinanceService/FinanceServiceTests/FinanceServiceInitializerTests.swift
@@ -0,0 +1,15 @@
+//
+//  FinanceServiceInitializerTests.swift
+//  FinanceServiceTests
+//
+
+import XCTest
+@testable import FinanceService
+
+final class FinanceServiceInitializerTests: XCTestCase {
+    
+    func testHellorWorld() {
+        XCTAssertEqual(FinanceServiceInitializer.helloWorld(), "Hello World from SDK!!")
+    }
+    
+}
