6abd07b882e3965430bb15936be111287bfa90de
Thonatas Reis Borges <premiersoft-thonatas.borges@ailos.coop.br>
2023-03-08 12:48:26 -0400

    Remove Initializer files and move UserProfile files to Microfrontend SDK

--- solutions/devsprint-julio-fernandes-10/NewLibraries/UserProfile/UserProfile/UserProfileTests/UserProfileTests.swift
+++ solutions/devsprint-julio-fernandes-10/NewLibraries/UserProfile/UserProfile/UserProfileTests/UserProfileTests.swift
@@ -6,10 +6,4 @@
 import XCTest
 @testable import UserProfile
 
-final class UserProfileTests: XCTestCase {
-    let sut = UserProfileInitializer(localizationString: "test ")
-    func test_helloWorld() {
-        let result = sut.helloWorld()
-        XCTAssertEqual(result, "test Hello World from FrontendModule!!")
-    }
-}
+final class UserProfileTests: XCTestCase { }
