ebda80e762022df34fc2709225a17c8f2baeb2df
Thonatas Reis Borges <premiersoft-thonatas.borges@ailos.coop.br>
2023-03-08 21:08:31 -0400

    Revert "Merge branch 'feature/ContactListMicrofrontend' into feature/TransfersMicrofrontend"
    This reverts commit 203fa793c2f25306d27c44371c1b21ed85c9ea0b, reversing
    changes made to de91009784361c0c4851760445514775a0e61207.

--- solutions/devsprint-julio-fernandes-10/Libraries/ContactList/ContactList/Classes/ContactListViewController.swift
+++ solutions/devsprint-julio-fernandes-10/Libraries/ContactList/ContactList/Classes/ContactListViewController.swift
@@ -6,24 +6,14 @@
 //
 
 import UIKit
-import ContactListInterface
 
-public class ContactListFactory: ContactListInterface {
-    public init() {}
-    
-    public func make() -> UIViewController {
-        return ContactListViewController()
-    }
-
-}
-
-protocol ContactListViewControllerDelegate: AnyObject {
+public protocol ContactListViewControllerDelegate: AnyObject {
     func didSelectContact()
 }
 
-class ContactListViewController: UIViewController {
+public class ContactListViewController: UIViewController {
 
-    weak var delegate: ContactListViewControllerDelegate?
+    public weak var delegate: ContactListViewControllerDelegate?
 
     lazy var contactListView: ContactListView = {
 
@@ -32,12 +22,12 @@ class ContactListViewController: UIViewController {
         return contactListView
     }()
 
-    override func loadView() {
+    public override func loadView() {
         super.loadView()
         self.view = contactListView
     }
 
-    override func viewDidLoad() {
+    public override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Contact List"
     }
